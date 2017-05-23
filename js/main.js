85a4e209ccbd8fd756a533e990266fe61a174d00
varadharajan <varadharajan@offlinetrend.com>
2017-05-23 18:44:44 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var app4 = new Vue({
+          el: '#myCarousel',
+          data: {
+            items: [
+              {text:"That's one small step for a man, one giant leap for mankind",author:"Neil Armstrong",images:"img/neilArmstrong.jpeg",colors:"#ee9003"},
+              {text:"The Universe is under no obligation to make sense to you",author:"Neil deGrasse Tyson",images:"img/slider2.jpg",colors:"#8e0e78"},
+              {text:"I would like to die on Mars. Just not on impact",author:"Elon Musk",images:"img/slider3.jpg",colors:"#8e0e78"},
+              {text:"If we can conquer space, we can conquer childhood hunger",author:"Buzz Aldrin",images:"img/slider4.jpg",colors:"#f04e4e"},
+              {text:"The eternal silence of these infinite spaces frightens me",author:"Blaise Pascal",images:"img/slider5.jpg",colors:"#34869e"}
+            ],
+            count:0
+          }
+   })
